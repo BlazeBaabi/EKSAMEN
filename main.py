931760296d94009d0849fbfb92870956e6e41133
BlazeBaabi <edvardseather@gmail.com>
2026-06-15 11:15:04 +0200

    la til inlogging og brukere

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
+brukere = None
+
+def lasteBrukere():
+    global brukere
+
+    with open("brukere.json", "r") as file:
+        data = json.load(file)
+        if data:
+            brukere = data
+        else:
+            brukere = {}
+
+lasteBrukere()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    global brukere
+
+    brukernavn = request.form.get("brukernavn")
+    passord = request.form.get("passord")
+
+    try:
+        bruker = brukere[brukernavn]
+        print(bruker)
+        #rolle = bruker["rolle"]
+        #profilBilde = bruker["profilBilde"]
+        if bruker:
+            if bruker["passord"] == passord:
+                return render_template("hjemmeside.html", brukernavn=brukernavn)#, rolle=rolle, profilBilde=profilBilde)
+            else:
+                return render_template("login.html", feil="Feil kode")
+        else:
+            return render_template("login.html", feil="Bruker ikke funnet")
+    except:
+        return render_template("login.html", feil="Bruker ikke funnet")
+
+app.run(debug=True)
